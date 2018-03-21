634a5719e4cfd94d68ae9371cde36fd463e0f182
Ilya Ruzakov <ilya.ruzz@gmail.com>
2018-03-21 14:48:19 +0500

    Updated ecs-deploy

--- bin/ecs_deploy.sh
+++ bin/ecs_deploy.sh
@@ -6,4 +6,4 @@ CLUSTER_NAME=ECS_CLUSTER_${TRAVIS_BRANCH_UPPERCASE}
 SERVICE_NAME=ECS_SERVICE_${TRAVIS_BRANCH_UPPERCASE}
 
 echo "Deploying $TRAVIS_BRANCH on service ${!SERVICE_NAME} on cluster ${!CLUSTER_NAME} with image $REMOTE_IMAGE_URL:$TRAVIS_BRANCH"
-${SCRIPTDIR}/../lib/ecs-deploy -t 300 -n ${!SERVICE_NAME} -c ${!CLUSTER_NAME} -r $AWS_REGION -i $REMOTE_IMAGE_URL:$TRAVIS_BRANCH --max-definitions 1 --desired-count 1 --enable-rollback --verbose 
+${SCRIPTDIR}/../lib/ecs-deploy -t 300 -n ${!SERVICE_NAME} -c ${!CLUSTER_NAME} -r $AWS_REGION -i $REMOTE_IMAGE_URL:$TRAVIS_BRANCH --max-definitions 1 --desired-count 1 --enable-rollback
