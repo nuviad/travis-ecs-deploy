8110cd147fcf881a1e5a3a066aa9c54fbbdaa27b
Ilya Ruzakov <ilya.ruzz@gmail.com>
2018-05-29 14:07:02 +0500

    Always use `latest` tag in Docker image and keep all task definitions

--- bin/ecs_deploy.sh
+++ bin/ecs_deploy.sh
@@ -5,5 +5,5 @@ TRAVIS_BRANCH_UPPERCASE=$(echo $TRAVIS_BRANCH | awk '{print toupper($0)}')
 CLUSTER_NAME=ECS_CLUSTER_${TRAVIS_BRANCH_UPPERCASE}
 SERVICE_NAME=ECS_SERVICE_${TRAVIS_BRANCH_UPPERCASE}
 
-echo "Deploying $TRAVIS_BRANCH on service ${!SERVICE_NAME} on cluster ${!CLUSTER_NAME} with image $REMOTE_IMAGE_URL:$TRAVIS_BRANCH"
-${SCRIPTDIR}/../lib/ecs-deploy -t 300 -n ${!SERVICE_NAME} -c ${!CLUSTER_NAME} -r $AWS_REGION -i $REMOTE_IMAGE_URL:$TRAVIS_BRANCH --max-definitions 1 --desired-count 2 --enable-rollback
+echo "Deploying $TRAVIS_BRANCH on service ${!SERVICE_NAME} on cluster ${!CLUSTER_NAME} with image $REMOTE_IMAGE_URL:latest"
+${SCRIPTDIR}/../lib/ecs-deploy -t 300 -n ${!SERVICE_NAME} -c ${!CLUSTER_NAME} -r $AWS_REGION -i $REMOTE_IMAGE_URL:latest --desired-count 2 --enable-rollback
